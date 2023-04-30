f9cc03f17744290e2fcbff8784d86ab1459a56d0
Brett Roper <brettroper22@gmail.com>
2023-04-30 02:07:41 -0700

    Green passed the first test for the bread

--- Bakery.Solution/Bakery/Models/Bread.cs
+++ Bakery.Solution/Bakery/Models/Bread.cs
@@ -4,12 +4,34 @@ using System;
 
 namespace BakeryBread
 {
-  class Bread
+  public class Bread
   {
-    static void BreadDiscount()
-    {
-
-    }
- 
+        public int HowMany { get; set; }
+        public int BreadPrice(int food, int price)
+        {      
+            while (food > 0)
+            { 
+                if (food >= 3)
+                {
+                    if((food % 3) == 0)
+                    {
+                        price += ((food / 3) * 10);
+                        food = 0;  
+                    }
+                    else
+                    {
+                        price = price + (((food -(food % 3)) / 3) * 10);
+                        food = (food % 3);
+                    }
+                }
+                else
+                {
+                    price = price + (food * 5);
+                    food = 0;
+                }
+            }
+            return price;
+        }
+        
   }
 }
