3a5a09c36e351d7222d0f679138423a6352c475c
Brett Roper <brettroper22@gmail.com>
2023-04-30 02:20:50 -0700

    Green pass on the donut formula

--- Bakery.Solution/Bakery/Models/Donut.cs
+++ Bakery.Solution/Bakery/Models/Donut.cs
@@ -4,12 +4,33 @@ using System;
 
 namespace BakeryDonut
 {
-  class Donut
+  public class Donut
   {
-    static void DonutDiscount()
-    {
-
-    }
- 
+    public int HowMany { get; set; }
+        public int DonutPrice(int food, int price)
+        {      
+             while (food > 0)
+            { 
+                if (food >= 3)
+                {
+                    if((food % 3) == 0)
+                    {
+                        price += ((food / 3) * 5);
+                        food = 0;  
+                    }
+                    else
+                    {
+                        price = price + (((food -(food % 3)) / 3) * 5);
+                        food = (food % 3);
+                    }
+                }
+                else
+                {
+                    price = price + (food * 2);
+                    food = 0;
+                }       
+            }
+            return price;
+        }
   }
 }
